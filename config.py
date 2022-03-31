2b52089c742302c4e15f44aed9e1250b5b2a2630
Tom Ha <31414852+tomha85@users.noreply.github.com>
2022-03-30 21:26:35 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or '[SQL_SERVER_GOES_HERE]'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or '[SQL_DATABASE_GOES_HERE]'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or '[SQL_USER_NAME_GOES_HERE]'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or '[SQL_PASSWORD_GOES_HERE]'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'sqlv2xserver.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'sqlv2x'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'sqlv2xadmin'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or '6782166137Tom'
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or '[BLOB_ACCOUNT_GOES_HERE]'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '[BLOB_STORAGE_KEY_GOES_HERE]'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or '[BLOB_CONTAINER_GOES_HERE]'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'utilityv2xstorage'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'le1SYnDACUxZunqNSDF0Im0UY8x8dlDoT/kh7ZOjyZSkl+W1RYl8isothP0rz/esINAxm0C5mZo/+AStqaRGcw=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'sdvreport'
